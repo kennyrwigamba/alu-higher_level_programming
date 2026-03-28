918e352c26617d7dcea5654b2f106c774b6dcf54
kennyrwigamba <kennyrwigamba@gmail.com>
2026-03-28 17:07:42 +0200

    Fix W503 in 6-square position validation

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -32,20 +32,17 @@ class Square:
     @position.setter
     def position(self, value):
         """Set square position with tuple validation."""
-        if (
-            type(value) is not tuple
-            or len(value) != 2
-            or type(value[0]) is not int
-            or type(value[1]) is not int
-            or value[0] < 0
-            or value[1] < 0
-        ):
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
         """Return the area of the square."""
-        return self.__size ** 2
+        return self.__size**2
 
     def my_print(self):
         """Print the square using # and applying position offsets."""
