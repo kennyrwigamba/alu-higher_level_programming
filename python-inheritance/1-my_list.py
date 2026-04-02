0c07986905235f09c1f4be783a45f983cb969be9
kennyrwigamba <kennyrwigamba@gmail.com>
2026-04-02 17:03:34 +0200

    Add method docstring for MyList.print_sorted

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,4 +6,5 @@ class MyList(list):
     """Custom list class with a method to print a sorted copy."""
 
     def print_sorted(self):
+        """Print the list elements in ascending sorted order."""
         print(sorted(self))
