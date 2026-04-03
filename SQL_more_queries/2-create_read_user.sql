2b0888321937a8b105b52bb1dff22dd9e617ce0e
kennyrwigamba <kennyrwigamba@gmail.com>
2026-04-03 10:37:48 +0200

    Fix SQL_more_queries privilege scripts

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,4 @@
--- Create hbtn_0d_2 and user_0d_2 with SELECT privilege only
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-CREATE USER 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+-- Create user_2_db and user_0d_2 with SELECT and INSERT privileges
+CREATE DATABASE IF NOT EXISTS user_2_db;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
