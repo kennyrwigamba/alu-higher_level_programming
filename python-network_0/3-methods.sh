0563a54f22071983607de40c37982bbea2a10a8d
kennyrwigamba <kennyrwigamba@gmail.com>
2026-04-03 21:11:50 +0200

    Fixed: 3_methods.sh

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Display all HTTP methods the server will accept
-curl -s -X OPTIONS -I "$1" \
-	| grep -i '^Allow:' \
-	| cut -d ' ' -f2- \
-	| tr -d '\r'
+# Display all HTTP methods the server of a given URL will accept.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
